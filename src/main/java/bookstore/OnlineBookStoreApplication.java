8ba307cee5bd0e34d53866fe715fe884ad2c0d56
Yura Novak <yuranovakr@gmail.com>
2023-10-17 18:06:21 +0300

    add entity

--- src/main/java/bookstore/OnlineBookStoreApplication.java
+++ src/main/java/bookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,39 @@
 package bookstore;
 
+import bookstore.model.Book;
+import bookstore.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book book = new Book();
+                book.setTitle("book1");
+                book.setAuthor("A B");
+                book.setIsbn("abc");
+                book.setPrice(BigDecimal.valueOf(300));
+                book.setDescription("good book");
+                book.setCoverImage("hors");
+                bookService.save(book);
+                System.out.println(bookService.findAll());
+            }
+        };
+    }
 }
